e599b5e22c5af959c8873543be6bfcfd821f6903
will-developer <william_limaa@hotmail.com>
2025-03-28 10:48:52 -0300

    feat: add PDF filtering for Anexos I and II

--- WebScraping/src/main.py
+++ WebScraping/src/main.py
@@ -9,9 +9,13 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 # Simple link detection 
 pdf_links = []
-for link in soup.find_all('a'):
+for link in soup.find_all('a', class_='internal-link'):
     href = link.get('href', '')
-    if href.endswith('.pdf'):
+    link_text = link.text.strip()
+    if href.endswith('.pdf') and ('Anexo I' in link_text or 'Anexo II' in link_text):
         pdf_links.append(href)
+        print(f'Found! {pdf_links}\n ------')
 
-print(f'Found {len(pdf_links)} PDF links')
+if not pdf_links:
+    print('No Anexos found!')
+    exit()
