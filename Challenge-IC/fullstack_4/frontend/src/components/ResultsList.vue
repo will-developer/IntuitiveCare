599fc2618f2026518d177b6b05771de9c6d29e28
will-developer <william_limaa@hotmail.com>
2025-03-31 20:08:41 -0300

    add ResultsList component structure

---
+++ Challenge-IC/fullstack_4/frontend/src/components/ResultsList.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+defineProps<{
+  results: any[]
+  isLoading: boolean
+  error: string | null
+}>()
+</script>
+
+<template>
+  <div class="results-display">
+    <div v-if="isLoading" class="loading">
+      Loading...
+    </div>
+    <div v-else-if="error" class="error-message">
+      <p><strong>Error:</strong> {{ error }}</p>
+    </div>
+    <div v-else class="results-container">
+      <p v-if="!results.length">Enter a search term to see results.</p>
+      <ul v-else class="results-list">
+        <li v-for="(item, index) in results" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loading {
+  text-align: center;
+  padding: 20px;
+  color: #555;
+  font-style: italic;
+}
+
+.error-message {
+  background-color: #ffebee;
+  color: #c62828;
+  border: 1px solid #e57373;
+  padding: 15px;
+  margin-top: 15px;
+  border-radius: 4px;
+  text-align: center;
+}
+
+.results-container {
+  margin-top: 20px;
+}
+
+.results-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.results-list li {
+  border: 1px solid #eee;
+  padding: 10px;
+  margin-bottom: 5px;
+  background-color: #fff;
+}
+</style>
