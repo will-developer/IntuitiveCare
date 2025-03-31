1e952b32f813ae969b93efa74532fa42989a9fba
will-developer <william_limaa@hotmail.com>
2025-03-31 11:09:29 -0300

    feat: add database connection test

--- Challenge-IC/DataBase-3/src/load_data.py
+++ Challenge-IC/DataBase-3/src/load_data.py
@@ -1,4 +1,26 @@
 import logging
+import mysql.connector
 
 #Log Configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'root', 
+    'database': 'ans_data'
+}
+
+def test_db_connection(db_config):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        print()
+        logging.info("Successfully connected to the database.\n")
+        connection.close()
+        return True
+    except mysql.connector.Error as e:
+        logging.error(f"Failed to connect to the database: {e}\n")
+        return False
+
+if __name__ == "__main__":
+    test_db_connection(DB_CONFIG)
