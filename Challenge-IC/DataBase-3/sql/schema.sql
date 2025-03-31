05232b7af192a761342fe8f620f579b46ee766c5
will-developer <william_limaa@hotmail.com>
2025-03-31 10:40:25 -0300

    feat(db): define schema for accounting table in schema.sql

--- Challenge-IC/DataBase-3/sql/schema.sql
+++ Challenge-IC/DataBase-3/sql/schema.sql
@@ -1,6 +1,4 @@
 -- Create operators table
-
--- Create the operators table
 CREATE TABLE operators (
     Registro_ANS INT PRIMARY KEY,
     CNPJ VARCHAR(18) NULL, -- Formato XX.XXX.XXX/XXXX-XX
@@ -22,4 +20,15 @@ CREATE TABLE operators (
     Cargo_Representante VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
     Regiao_de_Comercializacao VARCHAR(100) NULL,
     Data_Registro_ANS DATE NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Create accounting table
+CREATE TABLE accounting (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    trimestre_referencia DATE NOT NULL COMMENT
+    reg_ans INT NOT NULL COMMENT
+    cd_conta_contabil VARCHAR(50) NOT NULL COMMENT
+    descricao VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 
+    vl_saldo_inicial DECIMAL(18, 2) NULL COMMENT 
+    vl_saldo_final DECIMAL(18, 2) NULL COMMENT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
