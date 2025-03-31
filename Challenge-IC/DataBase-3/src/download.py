b86b32d0b2c936d822340a89f2b4cb9c7a5d911b
will-developer <william_limaa@hotmail.com>
2025-03-31 08:31:29 -0300

    fix: Save downloaded data to correct project directory

--- Challenge-IC/DataBase-3/src/download.py
+++ Challenge-IC/DataBase-3/src/download.py
@@ -1,17 +1,10 @@
+import logging
 import requests
 from pathlib import Path
 import os
-import logging
 
 #Log Configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-#Create Dir for place data
-def create_data_dir():
-    local_dir = Path(os.path.dirname(os.path.abspath(__file__)))
-    data_dir = local_dir.parent / "data" / "download"
-    data_dir.mkdir(parents=True, exist_ok=True)
-    return data_dir
 
 ######
 BASE_URL_ACCOUNTING = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/"
@@ -21,18 +14,20 @@ URL_OPERATORS_CSV = "https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano
 # Define project paths
 CURRENT_YEAR = 2025
 YEARS_TO_DOWNLOAD = [str(year) for year in range(CURRENT_YEAR - 2, CURRENT_YEAR)]
+
+DIR = Path(__file__).resolve().parent
 #Create Main Folder
-DATA_DIR = os.path.join("..", "data")
+DATA_DIR = DIR.parent / "data"
 #Accounting Folder and save files
-ACCOUNTING_DIR = os.path.join(DATA_DIR, "accounting")
-ZIPS_DIR = os.path.join(ACCOUNTING_DIR, "zips")
-CSVS_DIR = os.path.join(ACCOUNTING_DIR, "csvs")
+ACCOUNTING_DIR = DATA_DIR / "accounting"
+ZIPS_DIR = ACCOUNTING_DIR / "zips"
+CSVS_DIR = ACCOUNTING_DIR / "csvs"
 #Operators Folder and save files
-OPERATORS_DIR = os.path.join(DATA_DIR, "operators") 
-OPERATORS_CSV_PATH = os.path.join(OPERATORS_DIR, "operators.csv")
+OPERATORS_DIR = DATA_DIR / "operators"
+OPERATORS_CSV_PATH = OPERATORS_DIR / "operators.csv"
 
 logging.info(f"\nProject paths configured.")  
-logging.info(f"\nData directory: {os.path.abspath(DATA_DIR)}")
+logging.info(f"Data directory: {DATA_DIR.resolve()}")
 logging.info(f"\nYears to download: {YEARS_TO_DOWNLOAD}")
 
 def download_file(url, save_path, timeout=60):
